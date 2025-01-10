9229b07dd593953aa2541cfa4861f8ef75090ebf
Lukas Ellemunt <lukas.ellemunt@hti-services.net>
2025-01-10 15:48:41 +0000

    added guide for tests

---
+++ tests/assert_methods/test_custom.py
@@ -0,0 +1,19 @@
+# since we inherit from unittest.TestCase, we can use all the assert methods that unittest provides
+
+import unittest
+
+class CustomTestCase(unittest.TestCase):
+    def assertAllIntegers(self, values):
+        for value in values:
+            self.assertIsInstance(
+                value,
+                int,
+            )
+
+class TestIntegerList(CustomTestCase):
+    def test_values_are_integers(self):
+        integers_list = [1, 2, 3, 4, 5]
+        self.assertAllIntegers(integers_list)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/fixtures/test_stack.py
@@ -0,0 +1,47 @@
+# without the setup and teardown methods, we would need to create a new instance of the Stack class in each test method.
+# setup and teardown get called automatically by the unittest framework before and after each test method, respectively.
+
+# to use the same object in multiple test methods, we can leverage the class level fixtures setupclass and teardownclass.
+# see employee example for that
+
+import unittest
+
+from stack import Stack
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def tearDown(self):
+        del self.stack
+
+    def test_push(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.items, [1])
+
+    def test_pop(self):
+        self.stack.push(2)
+        item = self.stack.pop()
+        self.assertEqual(item, 2)
+
+    def test_len(self):
+        self.stack.push(3)
+        self.stack.push(4)
+        self.assertEqual(len(self.stack), 2)
+
+    def test_iter(self):
+        items = [5, 6, 7]
+        for item in items:
+            self.stack.push(item)
+        for stack_item, test_item in zip(self.stack, items):
+            self.assertEqual(stack_item, test_item)
+
+    def test_reversed(self):
+        items = [5, 6, 7]
+        for item in items:
+            self.stack.push(item)
+        reversed_stack = reversed(self.stack)
+        self.assertEqual(list(reversed_stack), [7, 6, 5])
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
