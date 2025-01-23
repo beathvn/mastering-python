76bf93d6df16a9ef115d73bca0a5b12fd2fcdd3a
Lukas Ellemunt <lukas.ellemunt@hti-services.net>
2025-01-23 14:05:53 +0000

    fixed pre-commit install not working

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,5 @@
 pip install poetry
 poetry config virtualenvs.in-project true
 poetry install
+source $(poetry env info --path)/bin/activate # this makes sure we are inside the virtualenv
 pre-commit install
