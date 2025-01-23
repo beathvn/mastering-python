ec9b02b0bd1cc3588c3bcd0fb106aa96ac49f83c
Lukas Ellemunt <lukas.ellemunt@hti-services.net>
2025-01-23 15:54:54 +0000

    update dev container configuration and fix virtualenv activation in post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 pip install poetry
 poetry config virtualenvs.in-project true
 poetry install
-source $(poetry env info --path)/bin/activate # this makes sure we are inside the virtualenv
+source .venv/bin/activate
 pre-commit install
